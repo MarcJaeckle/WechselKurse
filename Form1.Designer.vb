8958c725c53fd5f4a3f73f4a6f07cf50437ebdb6
Marc Jäckle <marcjaeckle@web.de>
2018-07-12 22:38:05 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.DateiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.BeendenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.BearbeitenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.LokalAktualisierenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.InternetAktualiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.AllowColumnReorder = True
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(0, 24)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.ShowGroups = False
+        Me.ListView1.Size = New System.Drawing.Size(384, 690)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DateiToolStripMenuItem, Me.BearbeitenToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(384, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'DateiToolStripMenuItem
+        '
+        Me.DateiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BeendenToolStripMenuItem})
+        Me.DateiToolStripMenuItem.Name = "DateiToolStripMenuItem"
+        Me.DateiToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.DateiToolStripMenuItem.Text = "&Datei"
+        '
+        'BeendenToolStripMenuItem
+        '
+        Me.BeendenToolStripMenuItem.Name = "BeendenToolStripMenuItem"
+        Me.BeendenToolStripMenuItem.Size = New System.Drawing.Size(120, 22)
+        Me.BeendenToolStripMenuItem.Text = "B&eenden"
+        '
+        'BearbeitenToolStripMenuItem
+        '
+        Me.BearbeitenToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LokalAktualisierenToolStripMenuItem, Me.InternetAktualiToolStripMenuItem})
+        Me.BearbeitenToolStripMenuItem.Name = "BearbeitenToolStripMenuItem"
+        Me.BearbeitenToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
+        Me.BearbeitenToolStripMenuItem.Text = "&Bearbeiten"
+        '
+        'LokalAktualisierenToolStripMenuItem
+        '
+        Me.LokalAktualisierenToolStripMenuItem.Name = "LokalAktualisierenToolStripMenuItem"
+        Me.LokalAktualisierenToolStripMenuItem.Size = New System.Drawing.Size(216, 22)
+        Me.LokalAktualisierenToolStripMenuItem.Text = "&Lokal Kurse aktualisieren"
+        '
+        'InternetAktualiToolStripMenuItem
+        '
+        Me.InternetAktualiToolStripMenuItem.Name = "InternetAktualiToolStripMenuItem"
+        Me.InternetAktualiToolStripMenuItem.Size = New System.Drawing.Size(216, 22)
+        Me.InternetAktualiToolStripMenuItem.Text = "&Internet Kurse aktualisieren"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 714)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(400, 750)
+        Me.Name = "Form1"
+        Me.Text = "Wechselkursetabelle der EZB"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents DateiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BeendenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BearbeitenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LokalAktualisierenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InternetAktualiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
